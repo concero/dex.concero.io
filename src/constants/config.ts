b7c862062618b13a30c8d75d8e6960b0be5c7e5e
lufaque <95128636+lufaque@users.noreply.github.com>
2023-12-16 16:35:04 +0300

    new config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,31 +1,15 @@
-interface IConfig {
-	baseURL: string
-	headers: {
-		'Content-Type': string
-	}
-	CRYPTOPANIC_API_KEY: string
-	LIFI_INTEGRATOR: string
-	LIFI_FEES: string
-	RANGO_API_KEY: string
-	ENSO_API_KEY: string
-	POSTHOG_API_KEY: string
-	POSTHOG_HOST: string
-	NULL_ADDRESS: string
-	NULL_E_ADDRESS: string
-	WEB3_MODAL_PROJECT_ID: string
-}
-
-export const config: IConfig = {
-	baseURL: process.env.CONCERO_API_URL as string,
+export const config = {
+	baseURL: <string>process.env.CONCERO_API_URL,
 	headers: { 'Content-Type': 'application/json' },
-	CRYPTOPANIC_API_KEY: process.env.CRYPTOPANIC_API_KEY as string,
-	LIFI_INTEGRATOR: process.env.LIFI_INTEGRATOR as string,
-	LIFI_FEES: process.env.LIFI_FEES as string,
-	RANGO_API_KEY: process.env.RANGO_API_KEY as string,
-	ENSO_API_KEY: process.env.ENSO_API_KEY as string,
-	POSTHOG_API_KEY: process.env.POSTHOG_API_KEY as string,
-	POSTHOG_HOST: process.env.POSTHOG_HOST as string,
+	CRYPTOPANIC_API_KEY: <string>process.env.CRYPTOPANIC_API_KEY,
+	LIFI_INTEGRATOR: <string>process.env.LIFI_INTEGRATOR,
+	LIFI_FEES: <string>process.env.LIFI_FEES,
+	RANGO_API_KEY: <string>process.env.RANGO_API_KEY,
+	ENSO_API_KEY: <string>process.env.ENSO_API_KEY,
+	POSTHOG_API_KEY: <string>process.env.POSTHOG_API_KEY,
+	POSTHOG_HOST: <string>process.env.POSTHOG_HOST,
 	NULL_ADDRESS: '0x0000000000000000000000000000000000000000',
 	NULL_E_ADDRESS: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
-	WEB3_MODAL_PROJECT_ID: process.env.WEB3_MODAL_PROJECT_ID as string,
+	WEB3_MODAL_PROJECT_ID: process.env.WEB3_MODAL_PROJECT_ID,
+	REFERRAL_PROGRAM_ADDRESS: <string>process.env.REFERRAL_PROGRAM_ADDRESS,
 }
